8d496080a798bcf10ae2da42c84dec88597c0238
Eric. R. Suárez <eric.ramos.suarez@gmail.com>
2021-04-04 15:10:59 +0100

    changees

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+var app = document.getElementById('title');
+
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(1500)
+  .deleteAll()
+  .typeString('eric.ramos@github:~$ sudo su')
+  .pauseFor(1500)
+  .deleteChars(10)
+  .typeString('#')
+  .start();
+  
+  
